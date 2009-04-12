0dffae541a3c488c381757472436b070d6f808d5
blindgaenger <blindgaenger@wohnkino.(none)>
2009-04-12 22:32:39 +0200

    moving to apfelsaft

--- app.rb
+++ app.rb
@@ -1,28 +1,38 @@
 require 'rubygems'
 require 'sinatra'
-
+require 'rdiscount'
 
 class Item
-  attr_reader :name, :icon
-  def initialize(name, icon)
+  attr_reader :name, :icon, :text
+  def initialize(name, icon, text=nil)
     @name = name
     @icon = icon
+    @text = text
+  end
+
+  def to_html
+    RDiscount.new(text || 'Hello').to_html
   end
 end
 
-get '/about' do
-  haml :about
-end
-
-get '/' do
-  @items = [
+def items
+   [
+    Item.new('Blog', 'avatar.png', "Check out his [Blog](http://blog.blindgaenger.net/)"),
     Item.new('GPS', 'com.apple.Maps.png'),
     Item.new('Profile', 'com.apple.MobileAddressBook.png'),
     Item.new('Mail', 'com.apple.mobilemail.png'),
     Item.new('Development', 'com.apple.Preferences.png'),
-    Item.new('Me', 'avatar.png'),
     Item.new('Music', 'com.apple.Remote.png'),
   ]
+end
+
+get '/about' do
+  @items = items
+  haml :about
+end
+
+get '/' do
+  @items = items
   haml :index
 end
 
