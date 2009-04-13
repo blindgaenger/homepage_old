e7f15f6766900ecd81f2b31fc474a2c1f226885a
blindgaenger <blindgaenger@gmail.com>
2009-04-13 15:24:58 +0200

    with sidebar

--- app.rb
+++ app.rb
@@ -1,45 +1,22 @@
 require 'rubygems'
 require 'sinatra'
 require 'rdiscount'
+require 'yaml'
 
-class Item
-  attr_reader :name, :icon, :text
-  def initialize(name, icon, text=nil)
-    @name = name
-    @icon = icon
-    @text = text
+helpers do
+  def markdown(text)
+    RDiscount.new(text).to_html
   end
-
-  def to_html
-    RDiscount.new(text || 'Hello').to_html
-  end
-end
-
-def items
-   [
-    Item.new('blog', 'blog.png', 'Check out his [Blog](http://blog.blindgaenger.net/ "blindgaenger\'s blog - when you don\'t know where you @") about software development and self-evaluation.'),
-    Item.new('soup', 'soup.png', 'Everything else ends up in a [Soup](http://blindgaenger.soup.io/ "Taste the soup?") ... Yummy!'),
-    Item.new('facebook', 'facebook.png', 'I know how to use [Facebook](http://www.facebook.com/ "Book of faces.") and if you\'re a kind person I\'ll add you.'),
-
-    #Item.new('GPS', 'gps.png'),
-    #Item.new('Profile', 'profile.png'),
-    #Item.new('Mail', 'mail.png'),
-    #Item.new('Development', 'development.png'),
-    #Item.new('Music', 'music.png'),
-  ]
-end
-
-get '/about' do
-  @items = items
-  haml :about
 end
 
 get '/' do
-  @items = items
+  items = YAML.load(File.read('content.yml'))
+  @texts = items.select{|i| i.is_a? String}
+  @items = items.reject{|i| i.is_a? String}
   haml :index
 end
 
-get '/:style.css' do
+get '/stylesheets/:style.css' do
   content_type 'text/css'
   sass params['style'].to_sym
 end
