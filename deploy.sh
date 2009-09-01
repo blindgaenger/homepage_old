98df8f78612c5c75b1ff18c637d68d6c8e2ce2c7
blindgaenger <blindgaenger@gmail.com>
2009-09-02 01:14:37 +0200

    force commit for fast forward

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 heroku config:add PROFILE=bjuenger --app bjuenger-homepage
-git push heroku-bjuenger master
+git push heroku-bjuenger master --force
 
 heroku config:add PROFILE=blindgaenger --app blindgaenger-homepage
-git push heroku-blindgaenger master
+git push heroku-blindgaenger master --force
 
